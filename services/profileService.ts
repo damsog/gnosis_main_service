dc9a81759802cb2169214d1f6d0c3e956ee0b2c3
kaiser24 <damsog38@gmail.com>
2022-11-19 00:22:06 -0500

    Profile crud service

---
+++ services/profileService.ts
@@ -0,0 +1,65 @@
+import prisma from '../configurations/dbinit';
+import { Profile } from '@prisma/client';
+import { ProfileBaseDM, ProfileDM } from '../dataModels/ProfileDataModel';
+
+
+export class ProfileService {
+    static async getAll(): Promise<Profile[]> {
+        const profiles = await prisma.profile.findMany();
+        return profiles;
+    }
+
+    static async getById(id: string): Promise<Profile | null> {
+        const profile =  await prisma.profile.findFirst({
+            where: {
+                id: id
+            }
+        });
+
+        return profile;
+    }
+
+    static async getByName(name: string): Promise<Profile | null> {
+        const profile = await prisma.profile.findFirst({
+            where: {
+                name: name
+            }
+        });
+
+        return profile;
+    }
+
+    static async create(profile: ProfileBaseDM): Promise<Profile> {
+        const profileCreated = await prisma.profile.create({
+            data: {
+                ...profile
+            }
+        });
+
+        return profileCreated;
+    }
+    
+    static async update(id: string, profile: ProfileBaseDM): Promise<Profile | null> {
+        const profileUpdated = await prisma.profile.update({
+            where: {
+                id: id
+            },
+            data: {
+                ...profile
+            }
+        });
+
+        return profileUpdated;
+    }
+
+    static async delete(id: string): Promise<Profile | null> {
+        const profile = await prisma.profile.delete({
+            where: {
+                id: id
+            }
+        });
+
+        return profile;
+    }
+
+}
