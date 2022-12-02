a4b74e72fb6681178914a7dde6660719f2de8c41
kaiser24 <damsog38@gmail.com>
2022-12-02 16:57:14 -0500

    Encoding service sends an image or multiple images correctly

--- services/encodingService.ts
+++ services/encodingService.ts
@@ -1,43 +1,54 @@
 import axios from "axios";
-import { Files } from "formidable";
-import { PeerConnectionDM } from "../dataModels/PeerConnectionModel";
 import SftpService from "./sftpService";
 import Client from 'ssh2-sftp-client';
 import formdata from 'form-data';
+import path from "path";
 
 export class EncodingService{
-    static async encode(imagePaths: string | string[], sftpClient: Client): Promise<string[]> {
+    static async encode(imagePaths: string[], sftpClient: Client): Promise<string[]> {
         
         const sftpService = new SftpService(sftpClient);
 
         let faservice_response: any;
 
-        if(typeof imagePaths === 'string'){
+        // In case only one image is sent we need to call the single image endpoint
+        if(imagePaths.length === 1){
             // Get the image from the sftp server as a buffer
-            const buffer = await sftpService.download(imagePaths, undefined);
+            const buffer = await sftpService.download(imagePaths[0], undefined);
 
             let form = new formdata();
-            form.append('file', buffer, { filename: 'felipe1.jpg' });
+            form.append('file', buffer, { filename: path.basename(imagePaths[0]) });
 
-            // Setting configuration of the request
+            // single image endpoint
             var url = `http://${process.env.FACE_ANALYTICS_SERVER}:${process.env.FACE_ANALYTICS_PORT}/encoder/image`;
 
             // Requesting to the Recognition Service
            faservice_response = await axios.post(url, form, { headers: form.getHeaders() });
+
+        // In case more than one image is sent we need to call the batch endpoint
         }else{
-            // Get the images from the sftp server as a buffer
-            const buffers = await Promise.all(imagePaths.map(async (imagePath) => {
-                const buffer = await sftpService.download(imagePath, undefined);
-                return buffer;
+            // Get the images from the sftp server as a buffer, with their filenames.
+            const imageBuffersData = await Promise.all(imagePaths.map(async (imagePath) => {
+                const imageBuffer = await sftpService.download(imagePath, undefined);
+                const imageName = path.basename(imagePath);
+                return {
+                    buffer: imageBuffer,
+                    name: imageName
+                }
             }));
 
             let form = new formdata();
-            buffers.forEach((buffer, index) => {
-                form.append('file', buffer, { filename: `felipe${index}.jpg` });
-            });
 
-            // Setting configuration of the request
-            var url = `http://${process.env.FACE_ANALYTICS_SERVER}:${process.env.FACE_ANALYTICS_PORT}/encoder/image`;
+            // Add the images to the form
+            for(const imageBuffer of imageBuffersData){
+                form.append('files', imageBuffer.buffer, { filename: imageBuffer.name });
+            }
+            
+            // The name key
+            form.append('key', 'felipe');
+            
+            // batch endpoint
+            var url = `http://${process.env.FACE_ANALYTICS_SERVER}:${process.env.FACE_ANALYTICS_PORT}/encoder/images`;
 
             // Requesting to the Recognition Service
            faservice_response = await axios.post(url, form, { headers: form.getHeaders() });
