e49bf78f8402efdaae40e484e5f2d5432f156d2c
kaiser24 <damsog38@gmail.com>
2022-12-17 19:43:39 -0500

    Sending sdp info in a form for detection stream

--- services/detectionService.ts
+++ services/detectionService.ts
@@ -1,32 +1,26 @@
 import { PeerConnectionDM } from '../dataModels/PeerConnectionModel';
 import axios from 'axios';
 import logger from '../lib/logger';
+import formdata from 'form-data';
 
 export class DetectionService{
     static async startDetectionStream(offerSdp: string, offerType: string): Promise<PeerConnectionDM> {
-        var payload = JSON.stringify({ 
-            "sdp" : offerSdp, 
-            "sdp_type" : offerType
-        });
+        let form = new formdata();
+
+        // Adding the sdp to the form
+        form.append('sdp', offerSdp);
+        form.append('sdp_type', offerType);
 
         // Setting configuration of the request
         var url = `http://${process.env.FACE_ANALYTICS_SERVER}:${process.env.FACE_ANALYTICS_PORT}/detector/stream`;
-        var config = {
-            method: 'post',
-            url: url,
-            headers: {'Content-Type':'application/json'},
-            data: payload
-        };
 
         // Requesting to the Recognition Service
         // Getting response for the signaling
-        const faservice_response = await axios(config).then((result) =>{
-            return result.data;
-        });
+        const faservice_response = await axios.post(url, form, { headers: form.getHeaders() });
 
         const answerSdp = {
-            "sdp" : faservice_response["sdp"],
-            "type" : faservice_response["type"]
+            "sdp" : faservice_response.data["sdp"],
+            "type" : faservice_response.data["type"]
         };
 
         return answerSdp;
