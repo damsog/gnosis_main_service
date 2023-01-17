adf6d22d62abd2aaeda08f6f36b11eabb51ecde6
kaiser24 <damsog38@gmail.com>
2023-01-17 18:44:30 -0500

    v1.3.0

--- index.ts
+++ index.ts
@@ -40,7 +40,7 @@ const swaggerOptions = {
         openapi: "3.0.1",
         info: {
             title: 'Platform API',
-            version: "1.2.0",
+            version: "1.3.0",
             description: 'Gnosis platform API'
         },
         servers: [
